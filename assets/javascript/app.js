c50995c8e80a8ed44ff698a8f0f3aa97ed175fa9
Alex Ogilvie <ogilvie1231@gmail.com>
2019-10-04 09:24:45 -0600

    Bringing values into the global scope and adjusting the query URL.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -42,18 +42,34 @@ $(document).on('click', '.button', function() {
 
 
 });
-console.log('Longshot: ', this.seller);
+console.log('Longshot: ', database);
 
 database.ref().on('child_added', function(childSnapshot) {
     console.log('firebase callback full: ', childSnapshot.val());
-    console.log('firebase callback buyer: ', childSnapshot.val().seller);
-    console.log('firebase callback seller: ', childSnapshot.seller);
+    console.log('firebase callback buyer: ', childSnapshot.val().buyer);
+    console.log('firebase callback seller: ', childSnapshot.val().seller);
 
     console.log('firebase callback seller street: ', childSnapshot.val().seller.street);
 });
 
+var buyer = {
+
+    street: $('#buyerAddress').val().trim(),
+    city: $('#buyerCity').val().trim(),
+    state: $('#buyerState').val().trim(),
+    zip: $('#buyerZip').val().trim(),
+};
+
+var seller = {
+
+    street: $('#sellerAddress').val().trim(),
+    city: $('#sellerCity').val().trim(),
+    state: $('#sellerState').val().trim(),
+    zip: $('#sellerZip').val().trim(),
+};
+
 L.mapquest.key = 'bR4IBmd5H6D8jaSYF4gzO12qVloc0MFi';
-var queryURL = "http://www.mapquestapi.com/geocoding/v1/address?key=" + L.mapquest.key + "&location=Salt+Lake+City,UT"
+var queryURL = "http://www.mapquestapi.com/geocoding/v1/address?key=" + L.mapquest.key + "&location=" + seller.street + seller.city + seller.zip
 
 
 $.ajax({
