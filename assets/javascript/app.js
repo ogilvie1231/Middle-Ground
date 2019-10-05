4b204de412c92ffea38dd1e8865ef3ac890b617d
Alex Ogilvie <ogilvie1231@gmail.com>
2019-10-05 10:19:52 -0600

    Removing firebase and objects.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,76 +1,25 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyA5dFq5gi2VyDNjmIf3KeyI3wlBShsf9og",
-    authDomain: "middle-ground-e79b1.firebaseapp.com",
-    databaseURL: "https://middle-ground-e79b1.firebaseio.com",
-    projectId: "middle-ground-e79b1",
-    storageBucket: "",
-    messagingSenderId: "567702054744",
-    appId: "1:567702054744:web:6ae32e19d18f467c05272d",
-    measurementId: "G-W1WTFZN1BT"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-var database = firebase.database();
-
 $(document).on('click', '.button', function() {
     event.preventDefault();
 
-    var buyer = {
 
-        street: $('#buyerAddress').val().trim(),
-        city: $('#buyerCity').val().trim(),
-        state: $('#buyerState').val().trim(),
-        zip: $('#buyerZip').val().trim(),
-    };
+    var buyerStreet = $('#buyerAddress').val().trim();
+    var buyerCity = $('#buyerCity').val().trim();
+    var buyerState = $('#buyerState').val().trim();
+    var buyerZip = $('#buyerZip').val().trim();
 
-    var seller = {
-
-        street: $('#sellerAddress').val().trim(),
-        city: $('#sellerCity').val().trim(),
-        state: $('#sellerState').val().trim(),
-        zip: $('#sellerZip').val().trim(),
-    };
-
-    database.ref().push({
-        buyer: buyer,
-        seller: buyer,
-    });
-
-    console.log('buyer info: ', buyer);
-    console.log('seller info: ', seller);
-
+    var sellerStreet = $('#sellerAddress').val().trim();
+    var sellerCity = $('#sellerCity').val().trim();
+    var sellerState = $('#sellerState').val().trim();
+    var sellerZip = $('#sellerZip').val().trim();
 
 });
 
-
-database.ref().on('child_added', function(childSnapshot) {
-    // console.log('firebase callback full: ', childSnapshot.val());
-    // console.log('firebase callback buyer: ', childSnapshot.val().buyer);
-    // console.log('firebase callback seller: ', childSnapshot.val().seller);
-
-    // console.log('firebase callback seller street: ', childSnapshot.val().seller.street);
-});
-
-var buyer = {
-
-    street: $('#buyerAddress').val().trim(),
-    city: $('#buyerCity').val().trim(),
-    state: $('#buyerState').val().trim(),
-    zip: $('#buyerZip').val().trim(),
-};
-
-var seller = {
-
-    street: $('#sellerAddress').val().trim(),
-    city: $('#sellerCity').val().trim(),
-    state: $('#sellerState').val().trim(),
-    zip: $('#sellerZip').val().trim(),
-};
 
 L.mapquest.key = 'bR4IBmd5H6D8jaSYF4gzO12qVloc0MFi';
 var queryURL = "http://www.mapquestapi.com/geocoding/v1/address?key=" + L.mapquest.key + "&location=utah"
     // http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=1600+Pennsylvania+Ave+NW,Washington,DC,20500
+console.log('query URL: ', queryURL)
+console.log('buyer state: ', buyerState)
 
 $.ajax({
         url: queryURL,
